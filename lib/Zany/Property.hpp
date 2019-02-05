e5bcf30a63e0f0c2a1b3d26e0cc400986b9bc305
Viguier Benjamin <benjamin.viguier@epitech.eu>
2019-02-05 17:53:06 +0100

    Fix property

--- lib/Zany/Property.hpp
+++ lib/Zany/Property.hpp
@@ -7,12 +7,21 @@
 
 #pragma once
 
+#include <typeinfo>
 #include <memory>
+#include <exception>
 
 namespace zany {
 
 class Property {
 public:
+	class Exception: public std::exception {
+	public:
+		~Exception() = default;
+		virtual const char* what() const noexcept final
+			{ return "Bad Property cast !"; }
+	};
+
 	Property() = default;
 	Property(Property const &) = default;
 	Property(Property &&) = default;
@@ -23,29 +32,45 @@ public:
 
 	template<typename _T, typename ..._Args>
 	static inline Property make(_Args &&...__args) {
-		using TargetType = typename _Data<_T>::type;
-		return Property(new _Data<_T>(TargetType(__args...)));
+		return Property(new _Data<_T>(std::forward<_Args>(__args)...));
 	}
 
 	template<typename _T, typename ..._Args>
 	inline _T &set(_Args &&...__args) {
-		using TargetType = typename _Data<_T>::type;
-		_bdata = decltype(_bdata)(new _Data<_T>(TargetType(__args...)));
+		_bdata = decltype(_bdata)(new _Data<_T>(std::forward<_Args>(__args)...));
 		return get<_T>();
 	}
 
-	template<typename _T> const _T	&get() const { return static_cast<_Data<_T>*>(_bdata.get())->data; }
-	template<typename _T> _T		&get() { return static_cast<_Data<_T>*>(_bdata.get())->data; }
+	inline const auto	&type() const { return _bdata->typeID; }
+
+	template<typename _T> const _T	&get() const {
+		if (type() != typeid(_T)) {
+			throw Exception();
+		}
+		return static_cast<_Data<_T>*>(_bdata.get())->data;
+	}
+	template<typename _T> _T		&get() {
+		if (type() != typeid(_T)) {
+			throw Exception();
+		}
+		return static_cast<_Data<_T>*>(_bdata.get())->data;
+	}
 private:
 	struct _BaseData {
+		template<typename _Arg> _BaseData(_Arg &&__typeID): typeID(__typeID) {}
 		virtual ~_BaseData() = default;
+
+		const std::type_info&	typeID;
 	};
 	template<typename _T> struct _Data: public _BaseData {
 		~_Data() = default;
 
 		using type = typename std::remove_const<_T>::type;
 
-		_Data(type &&d): data(d) {}
+		template<typename ..._Args>
+		_Data(_Args &&...__args):
+			_BaseData(typeid(type)),
+			data(std::forward<_Args>(__args)...) {}
 
 		type	data;
 	};
